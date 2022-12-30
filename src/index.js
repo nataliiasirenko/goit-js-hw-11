b7b3b119d74e64c5f609fafe1152bbfcee631774
nataliiasirenko <sirenko.nataliia@hotmail.com>
2022-12-30 15:33:57 +0000

    update get

--- src/index.js
+++ src/index.js
@@ -12,27 +12,44 @@ let photo = undefined;
 let pagesLeft = 0;
 const per_page = 40;
 
+const galleryMarkup = renderpicture(picture);
+console.log(galleryMarkup);
+
 formEll.addEventListener('submit', handleSubmit);
 loadMoreButtonEll.addEventListener('click', handleLoadMore);
 
 function renderpicture(picture) {
-  return `<div class="photo-card">
-          <a href=${picture.largeImageURL}><img src=${picture.webformatURL} alt=${picture.tags} loading="lazy" width=270px height=180px/>
+  return picture
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+          <a href=${largeImageURL}><img src=${webformatURL} alt=${tags} loading="lazy" width=270px height=180px/>
     <div class="info">
       <p class="info-item">
-          <b>Likes: ${picture.likes}</b>
+          <b>Likes: ${likes}</b>
       </p>
       <p class="info-item">
-          <b>Views: ${picture.views}</b>
+          <b>Views: ${views}</b>
       </p>
       <p class="info-item">
-          <b>Comments: ${picture.comments}</b>
+          <b>Comments: ${comments}</b>
       </p>
       <p class="info-item">
-           <b>Downloads: ${picture.downloads}</b>
+           <b>Downloads: ${downloads}</b>
       </p>
     </div></a>
   </div>`;
+      }
+    )
+    .join(``);
 }
 
 function getImg(photo, page) {
@@ -40,3 +57,7 @@ function getImg(photo, page) {
     `https://pixabay.com/api/?key=32455258-b6b5e3b19a045052743e3591c&q=${photo}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`
   );
 }
+
+function handleSubmit(params) {}
+
+function handleLoadMore(params) {}
