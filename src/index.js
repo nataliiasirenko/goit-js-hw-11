e12b69268effd0ce505c390cb70bee9af8529e2a
nataliiasirenko <sirenko.nataliia@hotmail.com>
2022-12-30 16:56:27 +0000

    update gallerymarup

--- src/index.js
+++ src/index.js
@@ -12,13 +12,12 @@ let photo = undefined;
 let pagesLeft = 0;
 const per_page = 40;
 
-const galleryMarkup = renderpicture(picture);
-console.log(galleryMarkup);
+const galleryMarkup = markupPictures(picture);
 
-formEll.addEventListener('submit', handleSubmit);
-loadMoreButtonEll.addEventListener('click', handleLoadMore);
+formEll.addEventListener('submit', onFormSubmit);
+loadMoreButtonEll.addEventListener('click', onLoadMore);
 
-function renderpicture(picture) {
+function markupPictures(picture) {
   return picture
     .map(
       ({
@@ -52,12 +51,66 @@ function renderpicture(picture) {
     .join(``);
 }
 
-function getImg(photo, page) {
+galleryEll.insertAdjacentHTML('beforeend', markupPictures(picture));
+
+function getImages(photo, page) {
   return axios.get(
     `https://pixabay.com/api/?key=32455258-b6b5e3b19a045052743e3591c&q=${photo}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`
   );
 }
 
-function handleSubmit(params) {}
+async function onFormSubmit(e) {
+  e.preventDefault();
+  loadMoreButtonEll.classList.add('hidden');
+  loadMoreButtonEll.classList.remove('load-more');
 
-function handleLoadMore(params) {}
+  page = 1;
+  photo = e.currentTarget.elements.searchQuery.value;
+  galleryEll.innerHTML = '';
+  await getImages(photo, page).then(response => {
+    if (photo === ' ' || photo === '') {
+      Notiflix.Notify.failure('Please type search and try again.');
+      return;
+    }
+    pagesLeft = response.data.totalHits;
+    if (response.data.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    } else {
+      Notiflix.Notify.success(`Hooray! We found ${pagesLeft} images.`);
+      galleryEll.insertAdjacentHTML(
+        'beforeend',
+        response.data.hits.galleryMarkup
+      );
+      pagesLeft -= per_page;
+      loadMoreButtonEll.classList.remove('hidden');
+      loadMoreButtonEll.classList.add('load-more');
+    }
+  });
+  lightBox.refresh();
+}
+
+async function onLoadMore() {
+  page += 1;
+  if (pagesLeft <= 0) {
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  } else {
+    await getImages(photo, page).then(response =>
+      galleryEll.insertAdjacentHTML(
+        'beforeend',
+        response.data.hits.galleryMarkup
+      )
+    );
+    pagesLeft -= per_page;
+  }
+  lightBox.refresh();
+}
+
+const lightBox = new SimpleLightBox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
